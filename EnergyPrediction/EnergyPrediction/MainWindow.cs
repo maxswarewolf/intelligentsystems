7707cd34600088ae1ae9e9adeededaa2215f15b3
Adrian pellegrino <adrian941024@gmail.com>
2016-08-29 12:11:32 +1000

    update

--- EnergyPrediction/EnergyPrediction/MainWindow.cs
+++ EnergyPrediction/EnergyPrediction/MainWindow.cs
@@ -37,13 +37,16 @@ using GeneticSharp.Domain.Selections;
 using GeneticSharp.Domain.Reinsertions;
 using GeneticSharp.Domain.Terminations
 
-public partial class MainWindow : Gtk.Window {
-	public MainWindow () : base ( Gtk.WindowType.Toplevel ) {
-		Build ();
-		}
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
 
-	protected void OnDeleteEvent ( object sender, DeleteEventArgs a ) {
-		Application.Quit ();
-		a.RetVal = true;
-		}
-	}
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
--- EnergyPrediction/EnergyPrediction/Program.cs
+++ EnergyPrediction/EnergyPrediction/Program.cs
@@ -28,13 +28,16 @@
 using System;
 using Gtk;
 
-namespace EnergyPrediction {
-	class MainClass {
-		public static void Main ( string [] args ) {
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
-			}
-		}
-	}
+namespace EnergyPrediction
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
