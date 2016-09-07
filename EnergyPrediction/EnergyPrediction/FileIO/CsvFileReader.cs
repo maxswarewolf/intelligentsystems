f541f5b363253b85267b01ca2997927b17418d28
Adrian pellegrino <adrian941024@gmail.com>
2016-09-07 16:45:02 +1000

    AER Data Update

---
+++ EnergyPrediction/EnergyPrediction/FileIO/CsvFileReader.cs
@@ -0,0 +1,161 @@
+﻿//
+// MIT LICENSE
+//
+// CsvFileReader.cs
+//
+// Author:
+//       Adrian Pellegrino <adrianpellegrino.licensing@gmail.com>
+//
+// Copyright (c) 2016 Adrian Pellegrino
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace EnergyPrediction
+{
+    public class CsvFileReader : CsvFileCommon, IDisposable
+    {
+        private StreamReader Reader;
+        private string CurrLine;
+        private int CurrPos;
+        private EmptyLineBehavior EmptyLineBehavior;
+
+        public CsvFileReader(Stream stream,
+            EmptyLineBehavior emptyLineBehavior = EmptyLineBehavior.NoColumns)
+        {
+            Reader = new StreamReader(stream);
+            EmptyLineBehavior = emptyLineBehavior;
+        }
+
+        public CsvFileReader(string path,
+            EmptyLineBehavior emptyLineBehavior = EmptyLineBehavior.NoColumns)
+        {
+            Reader = new StreamReader(path);
+            EmptyLineBehavior = emptyLineBehavior;
+        }
+
+        public bool ReadRow(List<string> columns)
+        {
+            if (columns == null)
+                throw new ArgumentNullException("columns");
+
+            ReadNextLine:
+            CurrLine = Reader.ReadLine();
+            CurrPos = 0;
+            if (CurrLine == null)
+                return false;
+            if (CurrLine.Length == 0)
+            {
+                switch (EmptyLineBehavior)
+                {
+                    case EmptyLineBehavior.NoColumns:
+                        columns.Clear();
+                        return true;
+                    case EmptyLineBehavior.Ignore:
+                        goto ReadNextLine;
+                    case EmptyLineBehavior.EndOfFile:
+                        return false;
+                }
+            }
+
+            string column;
+            int numColumns = 0;
+            while (true)
+            {
+                if (CurrPos < CurrLine.Length && CurrLine[CurrPos] == Quote)
+                    column = ReadQuotedColumn();
+                else
+                    column = ReadUnquotedColumn();
+                if (numColumns < columns.Count)
+                    columns[numColumns] = column;
+                else
+                    columns.Add(column);
+                numColumns++;
+                if (CurrLine == null || CurrPos == CurrLine.Length)
+                    break;
+                Debug.Assert(CurrLine[CurrPos] == Delimiter);
+                CurrPos++;
+            }
+            if (numColumns < columns.Count)
+                columns.RemoveRange(numColumns, columns.Count - numColumns);
+            return true;
+        }
+
+        private string ReadQuotedColumn()
+        {
+            Debug.Assert(CurrPos < CurrLine.Length && CurrLine[CurrPos] == Quote);
+            CurrPos++;
+
+            StringBuilder builder = new StringBuilder();
+            while (true)
+            {
+                while (CurrPos == CurrLine.Length)
+                {
+                    CurrLine = Reader.ReadLine();
+                    CurrPos = 0;
+                    if (CurrLine == null)
+                        return builder.ToString();
+                    builder.Append(Environment.NewLine);
+                }
+                if (CurrLine[CurrPos] == Quote)
+                {
+                    int nextPos = (CurrPos + 1);
+                    if (nextPos < CurrLine.Length && CurrLine[nextPos] == Quote)
+                        CurrPos++;
+                    else
+                        break;
+                }
+
+                builder.Append(CurrLine[CurrPos++]);
+            }
+
+            if (CurrPos < CurrLine.Length)
+            {
+
+                Debug.Assert(CurrLine[CurrPos] == Quote);
+                CurrPos++;
+
+                builder.Append(ReadUnquotedColumn());
+            }
+
+            return builder.ToString();
+        }
+
+
+        private string ReadUnquotedColumn()
+        {
+            int startPos = CurrPos;
+            CurrPos = CurrLine.IndexOf(Delimiter, CurrPos);
+            if (CurrPos == -1)
+                CurrPos = CurrLine.Length;
+            if (CurrPos > startPos)
+                return CurrLine.Substring(startPos, CurrPos - startPos);
+            return String.Empty;
+        }
+        public void Dispose()
+        {
+            Reader.Dispose();
+        }
+    }
+}
+
---
+++ EnergyPrediction/EnergyPrediction/FileIO/CsvFileWriter.cs
@@ -0,0 +1,81 @@
+﻿//
+// MIT LICENSE
+//
+// CsvFileWriter.cs
+//
+// Author:
+//       Adrian Pellegrino <adrianpellegrino.licensing@gmail.com>
+//
+// Copyright (c) 2016 Adrian Pellegrino
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EnergyPrediction
+{
+    public class CsvFileWriter : CsvFileCommon, IDisposable
+    {
+        private StreamWriter Writer;
+        private string OneQuote = null;
+        private string TwoQuotes = null;
+        private string QuotedFormat = null;
+
+        public CsvFileWriter(Stream stream)
+        {
+            Writer = new StreamWriter(stream);
+        }
+
+        public CsvFileWriter(string path)
+        {
+            Writer = new StreamWriter(path);
+        }
+
+        public void WriteRow(List<string> columns)
+        {
+            if (columns == null)
+                throw new ArgumentNullException("columns");
+
+            if (OneQuote == null || OneQuote[0] != Quote)
+            {
+                OneQuote = String.Format("{0}", Quote);
+                TwoQuotes = String.Format("{0}{0}", Quote);
+                QuotedFormat = String.Format("{0}{{0}}{0}", Quote);
+            }
+
+            for (int i = 0; i < columns.Count; i++)
+            {
+                if (i > 0)
+                    Writer.Write(Delimiter);
+                if (columns[i].IndexOfAny(SpecialChars) == -1)
+                    Writer.Write(columns[i]);
+                else
+                    Writer.Write(QuotedFormat, columns[i].Replace(OneQuote, TwoQuotes));
+            }
+            Writer.WriteLine();
+        }
+
+        public void Dispose()
+        {
+            Writer.Dispose();
+        }
+    }
+}
+
