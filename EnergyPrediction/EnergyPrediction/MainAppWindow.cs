fb628096be151fcbd597b549e5396d39dd5834ba
SeanEGrinter <seanegrinter@gmail.com>
2016-09-20 18:20:34 +1000

    Commiting the new Nuget package for Eto.Forms, a wrapper for Gtk#

---
+++ EnergyPrediction/EnergyPrediction/MainAppWindow.cs
@@ -0,0 +1,65 @@
+﻿//
+// MIT LICENSE
+//
+// Window.cs
+//
+// Author:
+//       Katie Clark, Sean Grinter, Adrian Pellegrino <Energy Prediction>
+//
+// Copyright (c) 2016 Katie Clark, Sean Grinter, Adrian Pellegrino
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+
+// Can't figure out how to use this on windows, so I'm trying something else
+//using Gtk;
+using Eto.Forms;
+using Eto.Drawing;
+
+public class MainAppWindow : Form
+{
+    public MainAppWindow()
+    {
+        Title = "My Cross-Platform App";
+        ClientSize = new Size(200, 200);
+        Content = new Label { Text = "Hello World!" };
+    }
+}
+
+/*
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        DeleteEvent += delegate
+        {
+            Application.Quit();
+        };
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
+*/
