63824573a18d98541b015f83266d7580a09cbba5
SeanEGrinter <seanegrinter@gmail.com>
2016-10-15 21:40:47 +1100

    local commit before pulling most recent changes
    Includes oxy plot

--- EnergyPrediction/EnergyPrediction/UI/GraphImage.cs
+++ EnergyPrediction/EnergyPrediction/UI/GraphImage.cs
@@ -1,12 +1,40 @@
 ﻿using Eto.Forms;
 using Eto.Drawing;
+using Eto.OxyPlot;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace EnergyPrediction.UI
 {
     class GraphImage : Panel
     {
+        private PlotModel geneticGraphingModel;
         public GraphImage()
         {
+            geneticGraphingModel = new PlotModel { Title = "Results" };
+            geneticGraphingModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                MajorGridlineThickness = 3,
+                MinorGridlineThickness = 3,
+                MajorGridlineColor = OxyColors.DarkSlateGray,
+                MinorGridlineColor = OxyColors.LightGray,
+                MinorStep = 0.5,
+                MajorStep = 1.0
+            });
+            geneticGraphingModel.Axes.Add(new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                MajorGridlineThickness = 3,
+                MinorGridlineThickness = 3,
+                MajorGridlineColor = OxyColors.DarkSlateGray,
+                MinorGridlineColor = OxyColors.LightGray,
+                MinorStep = 0.5,
+                MajorStep = 1.0
+            });
+            var areaSeries = new AreaSeries();
+            geneticGraphingModel.Series.Add(areaSeries);
             BackgroundColor = Colors.White;
         }
     }
